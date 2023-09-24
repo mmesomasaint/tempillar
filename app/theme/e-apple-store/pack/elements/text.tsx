7afb25cb12ba5b1e2f70773d6a3b1c6dd8d35396
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-24 14:31:04 +0100

    Feat: Design the text sizes

---
+++ app/theme/e-apple-store/pack/elements/text.tsx
@@ -0,0 +1,35 @@
+export function TextHeader({children}: {children: React.ReactNode}) {
+  return (
+    <div>This is the header text: {children}</div>
+  )
+}
+
+export function TextIntro({children}: {children: React.ReactNode}) {
+  return (
+    <div>This is the introductory text: {children}</div>
+  )
+}
+
+export function TextMid({children}: {children: React.ReactNode}) {
+  return (
+    <div>This is the medium text: {children}</div>
+  )
+}
+
+export function TextBase({children}: {children: React.ReactNode}) {
+  return (
+    <div>This is the base text: {children}</div>
+  )
+}
+
+export function TextSmall({children}: {children: React.ReactNode}) {
+  return (
+    <div>This is the small text: {children}</div>
+  )
+}
+
+export function TextTiny({children}: {children: React.ReactNode}) {
+  return (
+    <div>This is the tiny text: {children}</div>
+  )
+}
