708f1730e3c81c8ef918dde1a07a0a7f049093fb
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-25 21:34:36 +0100

    Fix: Adjust style & format with prettier

--- app/theme/e-apple-store/pack/components/product/slider.tsx
+++ app/theme/e-apple-store/pack/components/product/slider.tsx
@@ -1,17 +1,26 @@
-import Image from "next/image"
-import { useState } from "react"
+import Image from 'next/image'
+import { useState } from 'react'
 
-export default function Slider({srcList}: {srcList: string[]}) {
+export default function Slider({ srcList }: { srcList: string[] }) {
   const [activeSrc, setActiveSrc] = useState<string>(srcList[0])
 
   return (
-    <div>
-      <div>
+    <div className='flex md:flex-col justify-center items-center gap-10'>
+      <div className='md:order-0'>
         <Image src={activeSrc} width={400} height={400} alt='main-image' />
       </div>
-      <div>{srcList.map((src, id) => (
-        <Image onClick={() => setActiveSrc(src)} key={src} src={src} width={50} height={50} alt={`thumbnail-${id+1}`} />
-      ))}</div>
+      <div className='flex flex-col md:flex-row justify-evenly items-center gap-6'>
+        {srcList.map((src, id) => (
+          <Image
+            onClick={() => setActiveSrc(src)}
+            key={src}
+            src={src}
+            width={50}
+            height={50}
+            alt={`thumbnail-${id + 1}`}
+          />
+        ))}
+      </div>
     </div>
   )
 }
