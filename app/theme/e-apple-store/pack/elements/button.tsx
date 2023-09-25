17fc52b41b6a2549f9ea786faf80a99c2e2e6ae3
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-25 13:41:24 +0100

    Fix: Format with prettier

--- app/theme/e-apple-store/pack/elements/button.tsx
+++ app/theme/e-apple-store/pack/elements/button.tsx
@@ -1,6 +1,30 @@
-export function ButtonLarge({large, full, outlinePrimary, fillPrimary, fillFaded, children}: {children: React.ReactNode, large?: boolean, full?: boolean, outlinePrimary?: boolean, fillPrimary?: boolean, fillFaded?: boolean}) {
+export function ButtonLarge({
+  large,
+  full,
+  outlinePrimary,
+  fillPrimary,
+  fillFaded,
+  children,
+}: {
+  children: React.ReactNode
+  large?: boolean
+  full?: boolean
+  outlinePrimary?: boolean
+  fillPrimary?: boolean
+  fillFaded?: boolean
+}) {
   return (
-    <button className={`leading-[0] text-base font-semibold p-2 rounded-2xl border w-fit ${large && 'text-xl'} ${full && 'w-full'} ${outlinePrimary && 'border-apple-store-pri text-apple-store-pri'} ${fillPrimary && 'bg-apple-store-pri border-apple-store-pri text-white'} ${fillFaded && 'bg-white text-apple-store-faded border-apple-store-faded'}`}>
+    <button
+      className={`leading-[0] text-base font-semibold p-2 rounded-2xl border w-fit ${
+        large && 'text-xl'
+      } ${full && 'w-full'} ${
+        outlinePrimary && 'border-apple-store-pri text-apple-store-pri'
+      } ${
+        fillPrimary && 'bg-apple-store-pri border-apple-store-pri text-white'
+      } ${
+        fillFaded && 'bg-white text-apple-store-faded border-apple-store-faded'
+      }`}
+    >
       {children}
     </button>
   )
