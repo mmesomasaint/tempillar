a33a828abfa49e195df188ae72f7a92dbdadeb45
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-25 06:34:14 +0100

    Feat: Design the text element styles

--- app/theme/e-apple-store/pack/elements/text.tsx
+++ app/theme/e-apple-store/pack/elements/text.tsx
@@ -1,35 +1,35 @@
-export function TextHeader({children}: {children: React.ReactNode}) {
+export function TextHeader({primary, faded, fadedMax, children}: {primary: boolean, faded: boolean, fadedMax: boolean, children: React.ReactNode}) {
   return (
-    <div>This is the header text: {children}</div>
+    <div className={`text-3xl font-semibold ${primary && 'bg-apple-store-pri'} ${faded && 'bg-apple-store-faded'} ${fadedMax && 'bg-apple-store-faded-max'}`}>{children}</div>
   )
 }
 
-export function TextIntro({children}: {children: React.ReactNode}) {
+export function TextIntro({primary, faded, fadedMax, children}: {primary: boolean, faded: boolean, fadedMax: boolean, children: React.ReactNode}) {
   return (
-    <div>This is the introductory text: {children}</div>
+    <div className={`text-2xl font-medium ${primary && 'bg-apple-store-pri'} ${faded && 'bg-apple-store-faded'} ${fadedMax && 'bg-apple-store-faded-max'}`}>This is the introductory text: {children}</div>
   )
 }
 
-export function TextMid({children}: {children: React.ReactNode}) {
+export function TextMid({primary, faded, fadedMax, children}: {primary: boolean, faded: boolean, fadedMax: boolean, children: React.ReactNode}) {
   return (
-    <div>This is the medium text: {children}</div>
+    <div className={`text-xl font-medium ${primary && 'bg-apple-store-pri'} ${faded && 'bg-apple-store-faded'} ${fadedMax && 'bg-apple-store-faded-max'}`}>This is the medium text: {children}</div>
   )
 }
 
-export function TextBase({children}: {children: React.ReactNode}) {
+export function TextBase({primary, faded, fadedMax, children}: {primary: boolean, faded: boolean, fadedMax: boolean, children: React.ReactNode}) {
   return (
-    <div>This is the base text: {children}</div>
+    <div className={`text-lg font-medium ${primary && 'bg-apple-store-pri'} ${faded && 'bg-apple-store-faded'} ${fadedMax && 'bg-apple-store-faded-max'}`}>This is the base text: {children}</div>
   )
 }
 
-export function TextSmall({children}: {children: React.ReactNode}) {
+export function TextSmall({primary, faded, fadedMax, children}: {primary: boolean, faded: boolean, fadedMax: boolean, children: React.ReactNode}) {
   return (
-    <div>This is the small text: {children}</div>
+    <div className={`text-base font-medium ${primary && 'bg-apple-store-pri'} ${faded && 'bg-apple-store-faded'} ${fadedMax && 'bg-apple-store-faded-max'}`}>This is the small text: {children}</div>
   )
 }
 
-export function TextTiny({children}: {children: React.ReactNode}) {
+export function TextTiny({primary, faded, fadedMax, children}: {primary: boolean, faded: boolean, fadedMax: boolean, children: React.ReactNode}) {
   return (
-    <div>This is the tiny text: {children}</div>
+    <div className={`text-xs font-medium ${primary && 'bg-apple-store-pri'} ${faded && 'bg-apple-store-faded'} ${fadedMax && 'bg-apple-store-faded-max'}`}>This is the tiny text: {children}</div>
   )
 }
