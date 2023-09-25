285577ca6e62fc052400b4132c2bac45078a2375
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-25 07:53:04 +0100

    Feat: Test text styles

--- app/theme/e-apple-store/page.tsx
+++ app/theme/e-apple-store/page.tsx
@@ -1,7 +1,12 @@
-import { TextHeader } from "./pack/elements/text"
+import { TextHeader } from './pack/elements/text'
 
 export default function AppleStore() {
   return (
-    <TextHeader primary>Welcome to the electronic apple store</TextHeader>
+    <div>
+      <TextHeader primary>Welcome to the electronic apple store</TextHeader>
+      <TextHeader>Welcome to the electronic apple store</TextHeader>
+      <TextHeader faded>Welcome to the electronic apple store</TextHeader>
+      <TextHeader fadedMax>Welcome to the electronic apple store</TextHeader>
+    </div>
   )
 }
