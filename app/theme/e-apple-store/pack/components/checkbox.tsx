81be85a7d0ef31fddeb402ae66bc98b8e1b01b2f
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-29 17:23:24 +0100

    Feat: Design the checkbox component

--- app/theme/e-apple-store/pack/components/checkbox.tsx
+++ app/theme/e-apple-store/pack/components/checkbox.tsx
@@ -1,5 +1,12 @@
+import { TextTiny } from "../elements/text";
+
 export default function CheckBox({setCheck, children}: {children: React.ReactNode, setCheck?: () => void}) {
   return (
-
+    <div className="flex justify-start items-center gap-2">
+      <div>
+        
+      </div>
+      <TextTiny>{children}</TextTiny>
+    </div>
   )
 }
