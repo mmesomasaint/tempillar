2194d14581cb2244cc3bb71f086b109287cbbd34
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-29 18:40:27 +0100

    Fix: Format with prettier

--- app/theme/e-apple-store/pack/components/checkbox.tsx
+++ app/theme/e-apple-store/pack/components/checkbox.tsx
@@ -1,14 +1,27 @@
-import {BsCheckLg} from 'react-icons/bs'
-import { TextTiny } from "../elements/text";
+import { BsCheckLg } from 'react-icons/bs'
+import { TextTiny } from '../elements/text'
 
-export default function CheckBox({check, setCheck, children}: {children: React.ReactNode, check?: boolean, setCheck?: (prev: boolean) => void}) {
+export default function CheckBox({
+  check,
+  setCheck,
+  children,
+}: {
+  children: React.ReactNode
+  check?: boolean
+  setCheck?: (prev: boolean) => void
+}) {
   const color = check ? 'apple-store-pri' : 'apple-store-faded'
 
   return (
-    <div className="flex justify-start items-center gap-2" onClick={() => setCheck && setCheck(!check)}>
-      <div className={`w-12 h-12 flex justify-center items-center border border-${color}`}>
+    <div
+      className='flex justify-start items-center gap-2'
+      onClick={() => setCheck && setCheck(!check)}
+    >
+      <div
+        className={`w-12 h-12 flex justify-center items-center border border-${color}`}
+      >
         <BsCheckLg className={`text-lg text-${color}`} />
-      </div> 
+      </div>
       <TextTiny>{children}</TextTiny>
     </div>
   )
