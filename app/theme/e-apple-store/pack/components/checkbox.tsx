ceacf3eb6708590c241601f446c88f918deee207
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-29 18:39:57 +0100

    Feat: Design the checkbox component

--- app/theme/e-apple-store/pack/components/checkbox.tsx
+++ app/theme/e-apple-store/pack/components/checkbox.tsx
@@ -1,10 +1,13 @@
+import {BsCheckLg} from 'react-icons/bs'
 import { TextTiny } from "../elements/text";
 
-export default function CheckBox({check, setCheck, children}: {children: React.ReactNode, check?: boolean, setCheck?: () => void}) {
+export default function CheckBox({check, setCheck, children}: {children: React.ReactNode, check?: boolean, setCheck?: (prev: boolean) => void}) {
+  const color = check ? 'apple-store-pri' : 'apple-store-faded'
+
   return (
-    <div className="flex justify-start items-center gap-2">
-      <div className={`w-12 h-12 flex justify-center items-center border ${check ? 'border-apple-store-pri' : 'border-apple-store-faded'}`}>
-        
+    <div className="flex justify-start items-center gap-2" onClick={() => setCheck && setCheck(!check)}>
+      <div className={`w-12 h-12 flex justify-center items-center border border-${color}`}>
+        <BsCheckLg className={`text-lg text-${color}`} />
       </div> 
       <TextTiny>{children}</TextTiny>
     </div>
