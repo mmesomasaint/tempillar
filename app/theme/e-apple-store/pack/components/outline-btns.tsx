981553d734311a387ffd3f170c7641c7e273d329
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-30 14:40:19 +0100

    Feat: Create the outline buttons component

--- app/theme/e-apple-store/pack/components/outline-btns.tsx
+++ app/theme/e-apple-store/pack/components/outline-btns.tsx
@@ -1,12 +1,17 @@
-export default function OutlineButtons({options}: {options: string[]}) {
-  return (
-    <div>
+export default function OutlineButtons({options, setOptions, col}: {options: {}, setOptions: (prev: {}) => void, col: boolean}) {
+  const setSelectedHandler = (value: boolean, key: string) => {
+    const newOptions = {...options, [key]: value}
+    setOptions(newOptions)
+  }
 
+  return (
+    <div className={`flex ${col && 'flex-col'} items-center justify-start gap-5`}>
+      {Object.keys(options).map((option => (<OutlineButton key={option} setSelected={(value) => setSelectedHandler(value, option)}>{option}</OutlineButton>))}
     </div>
   )
 }
 
-export function OutlineButton({large, selected, setSelected, children}: {selected: boolean, setSelected: (prev: boolean) => void, children: React.ReactNode, large?: boolean}) {
+export function OutlineButton({large, selected, setSelected, children}: {selected?: boolean, setSelected: (prev: boolean) => void, children: React.ReactNode, large?: boolean}) {
   return (
     <div
     onClick={() => setSelected(!selected)}
