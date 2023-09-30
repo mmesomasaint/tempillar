93a04dd7ab66d59b3974d17088b0310a01608f4c
Mmesoma Saint <mmesomasaint@gmail.com>
2023-09-30 12:04:30 +0100

    Fix: Create the single outline button

--- app/theme/e-apple-store/pack/components/outline-btns.tsx
+++ app/theme/e-apple-store/pack/components/outline-btns.tsx
@@ -1,5 +1,22 @@
-export default function OutlineButtons({children}: {children: React.ReactNode}) {
+export default function OutlineButtons({options}: {options: string[]}) {
   return (
-    <div></div>
+    <div>
+
+    </div>
+  )
+}
+
+export function OutlineButton({large, selected, setSelected, children}: {selected: boolean, setSelected: (prev: boolean) => void, children: React.ReactNode, large?: boolean}) {
+  return (
+    <div
+    onClick={() => setSelected(!selected)}
+    className={`leading-[0] text-base font-semibold my-2 p-4 rounded-2xl border border-apple-store-faded text-apple-store-faded ${
+      large && 'text-xl my-4 p-6 rounded-3xl'
+    } ${
+      selected && 'border-apple-store-pri text-apple-store-pri'
+    }`}
+  >
+      {children}
+    </div>
   )
 }
