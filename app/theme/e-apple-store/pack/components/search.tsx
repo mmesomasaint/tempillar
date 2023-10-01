7981c31394bc5c477c1a005dccc85ef86b08491b
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-01 05:40:23 +0100

    Fix: Format with prettier

--- app/theme/e-apple-store/pack/components/search.tsx
+++ app/theme/e-apple-store/pack/components/search.tsx
@@ -1,10 +1,13 @@
-import { BsStarFill } from "react-icons/bs"
+import { BsStarFill } from 'react-icons/bs'
 
 export function SearchMain() {
   return (
-    <div className="relative h-fit">
+    <div className='relative h-fit'>
       <BsStarFill className='text-sm z-10 absolute top-[50%] -translate-y-[50%] left-5 pointer-events-none text-apple-store-faded' />
-      <input className="w-full rounded-full p-3 pl-10 pr-2 placeholder:text-apple-store-faded placeholder:text-base placeholder:font-semibold text-apple-store-faded font-semibold text-base border border-apple-store-faded focus:outline-apple-store-pri" placeholder="Search Store" />
+      <input
+        className='w-full rounded-full p-3 pl-10 pr-2 placeholder:text-apple-store-faded placeholder:text-base placeholder:font-semibold text-apple-store-faded font-semibold text-base border border-apple-store-faded focus:outline-apple-store-pri'
+        placeholder='Search Store'
+      />
     </div>
   )
 }
