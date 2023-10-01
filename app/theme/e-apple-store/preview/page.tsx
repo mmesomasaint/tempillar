e857cc0ecb944a73161044710c9493becc53d125
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-01 23:17:06 +0100

    Fix: Deisgn the first section of the preview site

--- app/theme/e-apple-store/preview/page.tsx
+++ app/theme/e-apple-store/preview/page.tsx
@@ -6,17 +6,18 @@ import { TextIntro, TextLabel } from "../pack/elements/text";
 export default function Home() {
   return (
     <main>
-      <div className="flex">
-        <TextIntro>LOGO</TextIntro>
-        <div className="flex justify-around items-center gap-5">
+      <div className="flex border justify-between items-center gap-10 px-7 py-3">
+        <TextIntro primary>iStore</TextIntro>
+        <div className="grow flex justify-center items-center gap-5">
       <DropDown selected={'All'} items={['All', 'Macbook', 'iMac', 'iPhone', 'Airpod', 'iWatch']} />
       <SearchBarMain />
         </div>
-        <div className="flex justify-end items-center gap-1">
+        <div className="flex justify-end items-center gap-3">
           <BsStar className='text-apple-store-faded-max text-sm' />
           <BsStar className='text-apple-store-faded-max text-sm' />
           <BsStar className='text-apple-store-faded-max text-sm' />
           <TextLabel fadedMax>|</TextLabel>
+          <div className="w-7 h-7 rounded-full border border-apple-store-faded-max" />
         </div>
       </div>
     </main>
