ec7cb0280370cd69bd6df190182b9ce285e3c0eb
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-01 23:17:17 +0100

    Fix: Format with prettier

--- app/theme/e-apple-store/preview/page.tsx
+++ app/theme/e-apple-store/preview/page.tsx
@@ -1,23 +1,26 @@
-import { BsStar } from "react-icons/bs";
-import DropDown from "../pack/components/dropdown";
-import { SearchBarMain } from "../pack/components/searchbar";
-import { TextIntro, TextLabel } from "../pack/elements/text";
+import { BsStar } from 'react-icons/bs'
+import DropDown from '../pack/components/dropdown'
+import { SearchBarMain } from '../pack/components/searchbar'
+import { TextIntro, TextLabel } from '../pack/elements/text'
 
 export default function Home() {
   return (
     <main>
-      <div className="flex border justify-between items-center gap-10 px-7 py-3">
+      <div className='flex border justify-between items-center gap-10 px-7 py-3'>
         <TextIntro primary>iStore</TextIntro>
-        <div className="grow flex justify-center items-center gap-5">
-      <DropDown selected={'All'} items={['All', 'Macbook', 'iMac', 'iPhone', 'Airpod', 'iWatch']} />
-      <SearchBarMain />
+        <div className='grow flex justify-center items-center gap-5'>
+          <DropDown
+            selected={'All'}
+            items={['All', 'Macbook', 'iMac', 'iPhone', 'Airpod', 'iWatch']}
+          />
+          <SearchBarMain />
         </div>
-        <div className="flex justify-end items-center gap-3">
+        <div className='flex justify-end items-center gap-3'>
           <BsStar className='text-apple-store-faded-max text-sm' />
           <BsStar className='text-apple-store-faded-max text-sm' />
           <BsStar className='text-apple-store-faded-max text-sm' />
           <TextLabel fadedMax>|</TextLabel>
-          <div className="w-7 h-7 rounded-full border border-apple-store-faded-max" />
+          <div className='w-7 h-7 rounded-full border border-apple-store-faded-max' />
         </div>
       </div>
     </main>
