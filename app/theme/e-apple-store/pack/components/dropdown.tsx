9b4221de65e97cfe56bed9a04b3a2f4450ba2291
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-01 16:07:44 +0100

    Fix: Add the open state

--- app/theme/e-apple-store/pack/components/dropdown.tsx
+++ app/theme/e-apple-store/pack/components/dropdown.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import {useState} from 'react'
 import { BsArrowDown } from 'react-icons/bs'
 import { TextLabel } from '../elements/text'
 
@@ -14,13 +15,15 @@ export default function DropDown({
   items: string[]
   full?: boolean
 }) {
+  const [open, setOpen] = useState(false)
+
   return (
-    <div className='inline-block relative text-apple-store-pri border border-apple-store-faded-max my-2 p-4 rounded-2xl'>
-      <div className='flex justify-start items-center gap-5'>
+    <div className={`inline-block relative text-apple-store-pri border ${open ? 'border-apple-store-pri' : 'border-apple-store-faded-max'} p-4 rounded-2xl w-[10%]`}>
+      <div className='flex justify-between items-center gap-5'>
         <TextLabel>{selected}</TextLabel>
-        <BsArrowDown className='text-sm text-apple-store-pri' />
+        <BsArrowDown className={`text-sm text-apple-store-pri ${open && 'rotate-180'}`} />
       </div>
-      <div className='absolute left-0 top-[100%] border-y border-apple-store-faded-max border-t-0 border-b-0 rounded-b-2xl flex flex-col'>
+      <div className={`${open ? 'block' : 'hidden'} w-full absolute left-0 top-[100%] border-y border-apple-store-faded-max border-t-0 border-b-0 rounded-b-2xl flex flex-col`}>
         {items.map((item) => (
           <DropItem
             isSelected={selected === item}
