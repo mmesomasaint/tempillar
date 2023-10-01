274f54a79b4b4775c63c3d7ee72ac71cbad5fa80
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-01 22:10:35 +0100

    Fix: Format with prettier

--- app/theme/e-apple-store/pack/components/accordion.tsx
+++ app/theme/e-apple-store/pack/components/accordion.tsx
@@ -1,15 +1,22 @@
 'use client'
 
-import {useState, ReactNode} from 'react'
+import { useState, ReactNode } from 'react'
 import { BiDownArrow } from 'react-icons/bi'
 import { TextLabel } from '../elements/text'
 
-export default function Accordion({children}: {children: ReactNode[] | ReactNode}) {
+export default function Accordion({
+  children,
+}: {
+  children: ReactNode[] | ReactNode
+}) {
   const [open, setOpen] = useState(false)
 
   return (
     <div className='w-full flex'>
-      <div className='flex justify-between items-center gap-5' onClick={() => setOpen(prev => !prev)}>
+      <div
+        className='flex justify-between items-center gap-5'
+        onClick={() => setOpen((prev) => !prev)}
+      >
         <TextLabel>Categories</TextLabel>
         <BiDownArrow className={`${open && 'rotate-180'} text-lg`} />
       </div>
