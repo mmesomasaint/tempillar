505a3c8a0eb1541f2d930b1d568a989a19547196
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-01 22:54:23 +0100

    Fix: Deisgn the first section of the preview site

--- app/theme/e-apple-store/preview/page.tsx
+++ app/theme/e-apple-store/preview/page.tsx
@@ -1,7 +1,24 @@
+import { BsStar } from "react-icons/bs";
+import DropDown from "../pack/components/dropdown";
+import { SearchBarMain } from "../pack/components/searchbar";
+import { TextIntro, TextLabel } from "../pack/elements/text";
+
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <main>
+      <div className="flex">
+        <TextIntro>LOGO</TextIntro>
+        <div className="flex justify-around items-center gap-5">
+      <DropDown selected={'All'} items={['All', 'Macbook', 'iMac', 'iPhone', 'Airpod', 'iWatch']} />
+      <SearchBarMain />
+        </div>
+        <div className="flex justify-end items-center gap-1">
+          <BsStar className='text-apple-store-faded-max text-sm' />
+          <BsStar className='text-apple-store-faded-max text-sm' />
+          <BsStar className='text-apple-store-faded-max text-sm' />
+          <TextLabel fadedMax>|</TextLabel>
+        </div>
+      </div>
+    </main>
   )
 }
