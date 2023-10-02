a58864b81b8168eb7e4d7cfe4d2499a5702a092f
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-02 08:35:46 +0100

    Feat: Improve on the styles

--- app/theme/e-apple-store/preview/page.tsx
+++ app/theme/e-apple-store/preview/page.tsx
@@ -7,7 +7,7 @@ import CheckBox from '../pack/components/checkbox'
 
 export default function Home() {
   return (
-    <main>
+    <main className='min-h-screen border border-red-500'>
       <div className='flex border-y border-apple-store-faded-max justify-between items-center gap-40 px-7 py-4'>
         <TextIntro primary>iStore</TextIntro>
         <div className='grow flex justify-center items-center gap-5'>
@@ -40,29 +40,29 @@ export default function Home() {
           </span>
           <TextLabel>Umuajonisi River, Port Harcourt</TextLabel>
         </div>
-        <div className='bg-apple-store-faded-max px-7 py-4 grid grid-cols-10 place-items-stretch gap-5'>
-          <div className='col-span-3 flex flex-col gap-10 bg-white rounded-xl border border-apple-store-faded-max'>
-            <TextMid>Filters</TextMid>
-            <Accordion title='Categories'>
-              <CheckBox check={false}>Airpods</CheckBox>
-              <CheckBox check={false}>iPhone</CheckBox>
-              <CheckBox check={false}>iPad</CheckBox>
-              <CheckBox check={false}>Macbook</CheckBox>
-              <CheckBox check={false}>Watch</CheckBox>
-            </Accordion>
-            <Accordion title='Condition'>
-              <CheckBox check={false}>New Stuff</CheckBox>
-              <CheckBox check={false}>Fairly Used</CheckBox>
-              <CheckBox check={false}>Second Hand</CheckBox>
-            </Accordion>
-            <Accordion title='Price'>Price</Accordion>
-            <Accordion title='Payment'>
-              <CheckBox check={false}>Cash on Delivery</CheckBox>
-              <CheckBox check={false}>Prepaid</CheckBox>
-              <CheckBox check={false}>iStore Coupon</CheckBox>
-              <CheckBox check={false}>Binance Pay</CheckBox>
-            </Accordion>
-          </div>
+      </div>
+      <div className='bg-gray-100 px-7 py-4 min-h-full grid grid-cols-10 gap-5'>
+        <div className='col-span-2 flex flex-col gap-10 bg-white rounded-xl border border-apple-store-faded-max'>
+          <TextMid>Filters</TextMid>
+          <Accordion title='Categories'>
+            <CheckBox check={false}>Airpods</CheckBox>
+            <CheckBox check={false}>iPhone</CheckBox>
+            <CheckBox check={false}>iPad</CheckBox>
+            <CheckBox check={false}>Macbook</CheckBox>
+            <CheckBox check={false}>Watch</CheckBox>
+          </Accordion>
+          <Accordion title='Condition'>
+            <CheckBox check={false}>New Stuff</CheckBox>
+            <CheckBox check={false}>Fairly Used</CheckBox>
+            <CheckBox check={false}>Second Hand</CheckBox>
+          </Accordion>
+          <Accordion title='Price'>Price</Accordion>
+          <Accordion title='Payment'>
+            <CheckBox check={false}>Cash on Delivery</CheckBox>
+            <CheckBox check={false}>Prepaid</CheckBox>
+            <CheckBox check={false}>iStore Coupon</CheckBox>
+            <CheckBox check={false}>Binance Pay</CheckBox>
+          </Accordion>
         </div>
       </div>
     </main>
