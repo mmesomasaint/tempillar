e42244ee679c6d0136a65f70b572151d839f439a
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-02 11:04:13 +0100

    Feat: Create a mock data

---
+++ app/theme/e-apple-store/preview/lib/products.ts
@@ -0,0 +1,103 @@
+interface Product {
+  title: string
+  variants: string[]
+  src: string
+  price: number
+  discount: number
+  colors: string[]
+  rating: number
+  amountSold: number
+}
+
+const products: Product[] = [
+  {
+    title: 'Apple Macbook Air M1',
+    variants: ['256GB', '512GB'],
+    src: '/imgs/macbook-pro-fv-2.jpeg',
+    price: 1352,
+    discount: 2585,
+    colors: ['lime', 'gray', 'zinc'],
+    rating: 4.8,
+    amountSold: 680
+  },
+  {
+    title: 'Apple Macbook Air M1',
+    variants: ['256GB', '512GB'],
+    src: '/imgs/macbook-pro-fv-2.jpeg',
+    price: 1352,
+    discount: 2585,
+    colors: ['lime', 'gray', 'zinc'],
+    rating: 4.8,
+    amountSold: 680
+  },
+  {
+    title: 'Apple Macbook Air M1',
+    variants: ['256GB', '512GB'],
+    src: '/imgs/macbook-pro-fv-2.jpeg',
+    price: 1352,
+    discount: 2585,
+    colors: ['lime', 'gray', 'zinc'],
+    rating: 4.8,
+    amountSold: 680
+  },
+  {
+    title: 'Apple Macbook Air M1',
+    variants: ['256GB', '512GB'],
+    src: '/imgs/macbook-pro-fv-2.jpeg',
+    price: 1352,
+    discount: 2585,
+    colors: ['lime', 'gray', 'zinc'],
+    rating: 4.8,
+    amountSold: 680
+  },
+  {
+    title: 'Apple Macbook Air M1',
+    variants: ['256GB', '512GB'],
+    src: '/imgs/macbook-pro-fv-2.jpeg',
+    price: 1352,
+    discount: 2585,
+    colors: ['lime', 'gray', 'zinc'],
+    rating: 4.8,
+    amountSold: 680
+  },
+  {
+    title: 'Apple Macbook Air M1',
+    variants: ['256GB', '512GB'],
+    src: '/imgs/macbook-pro-fv-2.jpeg',
+    price: 1352,
+    discount: 2585,
+    colors: ['lime', 'gray', 'zinc'],
+    rating: 4.8,
+    amountSold: 680
+  },
+  {
+    title: 'Apple Macbook Air M1',
+    variants: ['256GB', '512GB'],
+    src: '/imgs/macbook-pro-fv-2.jpeg',
+    price: 1352,
+    discount: 2585,
+    colors: ['lime', 'gray', 'zinc'],
+    rating: 4.8,
+    amountSold: 680
+  },
+  {
+    title: 'Apple Macbook Air M1',
+    variants: ['256GB', '512GB'],
+    src: '/imgs/macbook-pro-fv-2.jpeg',
+    price: 1352,
+    discount: 2585,
+    colors: ['lime', 'gray', 'zinc'],
+    rating: 4.8,
+    amountSold: 680
+  },
+  {
+    title: 'Apple Macbook Air M1',
+    variants: ['256GB', '512GB'],
+    src: '/imgs/macbook-pro-fv-2.jpeg',
+    price: 1352,
+    discount: 2585,
+    colors: ['lime', 'gray', 'zinc'],
+    rating: 4.8,
+    amountSold: 680
+  },
+]
