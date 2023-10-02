2f7554d86b6f2b2acacf2189fd8b2a5207691f54
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-02 09:42:33 +0100

    Feat: Improve on the styles & use another icon

--- app/theme/e-apple-store/pack/components/searchbar.tsx
+++ app/theme/e-apple-store/pack/components/searchbar.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { BsStarFill } from 'react-icons/bs'
+import { BsStar } from 'react-icons/bs'
 import { TextLabel } from '../elements/text'
 
 export function SearchBarMain({
@@ -12,11 +12,11 @@ export function SearchBarMain({
 }) {
   return (
     <div className='relative h-fit w-full'>
-      <BsStarFill className='text-sm z-10 absolute top-[50%] -translate-y-[50%] left-5 pointer-events-none text-apple-store-faded' />
+      <BsStar className='text-sm z-10 absolute top-[50%] -translate-y-[50%] left-5 pointer-events-none text-apple-store-faded' />
       <input
         name='searchText'
         value={searchText}
-        className='w-full rounded-full p-3 pl-10 pr-2 placeholder:text-apple-store-faded placeholder:text-base placeholder:font-semibold text-apple-store-faded font-semibold text-base border border-apple-store-faded focus:outline-apple-store-pri'
+        className='w-full rounded-full p-3 pl-10 pr-2 placeholder:text-apple-store-faded placeholder:text-base placeholder:font-semibold text-apple-store-faded font-semibold text-base border border-apple-store-faded-max focus:outline-apple-store-pri'
         placeholder='Search Store'
         onChange={(e) => setSearchText && setSearchText(e.target.value)}
       />
