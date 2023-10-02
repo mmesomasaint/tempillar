49e23b22b531304428daf3b433cc13a5325432fa
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-02 08:15:15 +0100

    Feat: Design the filter section

--- app/theme/e-apple-store/preview/page.tsx
+++ app/theme/e-apple-store/preview/page.tsx
@@ -2,6 +2,8 @@ import { BsStar } from 'react-icons/bs'
 import DropDown from '../pack/components/dropdown'
 import { SearchBarMain } from '../pack/components/searchbar'
 import { TextIntro, TextLabel, TextMid, TextTiny } from '../pack/elements/text'
+import Accordion from '../pack/components/accordion'
+import CheckBox from '../pack/components/checkbox'
 
 export default function Home() {
   return (
@@ -41,6 +43,26 @@ export default function Home() {
         <div className='bg-apple-store-faded-max px-7 py-4 grid grid-cols-10 place-items-stretch gap-5'>
           <div className='col-span-3 flex flex-col gap-10 bg-white rounded-xl border border-apple-store-faded-max'>
             <TextMid>Filters</TextMid>
+            <Accordion title='Categories'>
+            <CheckBox check={false}>Airpods</CheckBox>
+            <CheckBox check={false}>iPhone</CheckBox>
+            <CheckBox check={false}>iPad</CheckBox>
+            <CheckBox check={false}>Macbook</CheckBox>
+            <CheckBox check={false}>Watch</CheckBox>
+            </Accordion>
+            <Accordion title='Condition'>
+            <CheckBox check={false}>New Stuff</CheckBox>
+            <CheckBox check={false}>Fairly Used</CheckBox>
+            <CheckBox check={false}>Second Hand</CheckBox>
+            </Accordion>
+            <Accordion title='Price'>
+            </Accordion>
+            <Accordion title='Payment'>
+            <CheckBox check={false}>Cash on Delivery</CheckBox>
+            <CheckBox check={false}>Prepaid</CheckBox>
+            <CheckBox check={false}>iStore Coupon</CheckBox>
+            <CheckBox check={false}>Binance Pay</CheckBox>
+            </Accordion>
           </div>
         </div>
       </div>
