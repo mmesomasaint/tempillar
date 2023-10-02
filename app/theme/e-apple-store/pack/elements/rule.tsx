5814a5609644ff345851ebee659be462a2897662
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-02 09:34:07 +0100

    Fix: Format with prettier

--- app/theme/e-apple-store/pack/elements/rule.tsx
+++ app/theme/e-apple-store/pack/elements/rule.tsx
@@ -1,3 +1,11 @@
-export function HR({children}: {children?: React.ReactNode}) {
-  return <div className={`border-b border-apple-store-faded-max w-full ${children && 'pb-3'}`}>{children}</div>
+export function HR({ children }: { children?: React.ReactNode }) {
+  return (
+    <div
+      className={`border-b border-apple-store-faded-max w-full ${
+        children && 'pb-3'
+      }`}
+    >
+      {children}
+    </div>
+  )
 }
--- app/theme/e-apple-store/preview/page.tsx
+++ app/theme/e-apple-store/preview/page.tsx
@@ -46,33 +46,33 @@ export default function Home() {
         <div className='col-span-2 flex flex-col gap-5 bg-white rounded-xl border border-apple-store-faded-max p-5'>
           <TextMid>Filters</TextMid>
           <HR>
-          <Accordion title='Categories' defaultOpen>
-            <CheckBox check={false}>Airpods</CheckBox>
-            <CheckBox check={false}>iPhone</CheckBox>
-            <CheckBox check={false}>iPad</CheckBox>
-            <CheckBox check={false}>Macbook</CheckBox>
-            <CheckBox check={false}>Watch</CheckBox>
-          </Accordion>
+            <Accordion title='Categories' defaultOpen>
+              <CheckBox check={false}>Airpods</CheckBox>
+              <CheckBox check={false}>iPhone</CheckBox>
+              <CheckBox check={false}>iPad</CheckBox>
+              <CheckBox check={false}>Macbook</CheckBox>
+              <CheckBox check={false}>Watch</CheckBox>
+            </Accordion>
           </HR>
           <HR>
-          <Accordion title='Condition' defaultOpen>
-            <CheckBox check={false}>New Stuff</CheckBox>
-            <CheckBox check={false}>Fairly Used</CheckBox>
-            <CheckBox check={false}>Second Hand</CheckBox>
-          </Accordion>
+            <Accordion title='Condition' defaultOpen>
+              <CheckBox check={false}>New Stuff</CheckBox>
+              <CheckBox check={false}>Fairly Used</CheckBox>
+              <CheckBox check={false}>Second Hand</CheckBox>
+            </Accordion>
           </HR>
           <HR>
-          <Accordion title='Price' defaultOpen>
-            Price
-          </Accordion>
+            <Accordion title='Price' defaultOpen>
+              Price
+            </Accordion>
           </HR>
           <HR>
-          <Accordion title='Payment' defaultOpen>
-            <CheckBox check={false}>Cash on Delivery</CheckBox>
-            <CheckBox check={false}>Prepaid</CheckBox>
-            <CheckBox check={false}>iStore Coupon</CheckBox>
-            <CheckBox check={false}>Binance Pay</CheckBox>
-          </Accordion>
+            <Accordion title='Payment' defaultOpen>
+              <CheckBox check={false}>Cash on Delivery</CheckBox>
+              <CheckBox check={false}>Prepaid</CheckBox>
+              <CheckBox check={false}>iStore Coupon</CheckBox>
+              <CheckBox check={false}>Binance Pay</CheckBox>
+            </Accordion>
           </HR>
         </div>
       </div>
