172a0872133b999bd49fd5c37db8f08b1225a3dd
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-02 22:00:11 +0100

    Feat: Improve on the styles

--- app/theme/e-apple-store/pack/components/outline-btns.tsx
+++ app/theme/e-apple-store/pack/components/outline-btns.tsx
@@ -46,8 +46,8 @@ export function OutlineButton({
   return (
     <div
       onClick={() => setSelected && setSelected(!selected)}
-      className={`inline-block my-2 p-4 rounded-3xl border ${
-        large && 'my-3 p-5 rounded-3xl'
+      className={`inline-block p-4 rounded-3xl border ${
+        large && 'p-5 rounded-3xl'
       } ${
         selected
           ? 'border-apple-store-pri text-apple-store-pri'
