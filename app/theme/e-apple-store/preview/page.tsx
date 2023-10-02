577bbc7e43a93c7b17c518f05e203e5c92d24951
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-02 08:15:25 +0100

    Fix: Format with prettier

--- app/theme/e-apple-store/preview/page.tsx
+++ app/theme/e-apple-store/preview/page.tsx
@@ -44,24 +44,23 @@ export default function Home() {
           <div className='col-span-3 flex flex-col gap-10 bg-white rounded-xl border border-apple-store-faded-max'>
             <TextMid>Filters</TextMid>
             <Accordion title='Categories'>
-            <CheckBox check={false}>Airpods</CheckBox>
-            <CheckBox check={false}>iPhone</CheckBox>
-            <CheckBox check={false}>iPad</CheckBox>
-            <CheckBox check={false}>Macbook</CheckBox>
-            <CheckBox check={false}>Watch</CheckBox>
+              <CheckBox check={false}>Airpods</CheckBox>
+              <CheckBox check={false}>iPhone</CheckBox>
+              <CheckBox check={false}>iPad</CheckBox>
+              <CheckBox check={false}>Macbook</CheckBox>
+              <CheckBox check={false}>Watch</CheckBox>
             </Accordion>
             <Accordion title='Condition'>
-            <CheckBox check={false}>New Stuff</CheckBox>
-            <CheckBox check={false}>Fairly Used</CheckBox>
-            <CheckBox check={false}>Second Hand</CheckBox>
+              <CheckBox check={false}>New Stuff</CheckBox>
+              <CheckBox check={false}>Fairly Used</CheckBox>
+              <CheckBox check={false}>Second Hand</CheckBox>
             </Accordion>
-            <Accordion title='Price'>
-            </Accordion>
+            <Accordion title='Price'></Accordion>
             <Accordion title='Payment'>
-            <CheckBox check={false}>Cash on Delivery</CheckBox>
-            <CheckBox check={false}>Prepaid</CheckBox>
-            <CheckBox check={false}>iStore Coupon</CheckBox>
-            <CheckBox check={false}>Binance Pay</CheckBox>
+              <CheckBox check={false}>Cash on Delivery</CheckBox>
+              <CheckBox check={false}>Prepaid</CheckBox>
+              <CheckBox check={false}>iStore Coupon</CheckBox>
+              <CheckBox check={false}>Binance Pay</CheckBox>
             </Accordion>
           </div>
         </div>
