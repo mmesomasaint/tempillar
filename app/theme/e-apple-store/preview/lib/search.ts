037ec6a2ca9cdcc89d96e4b92bb16999463757e2
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-04 22:56:38 +0100

    Fix: Typo

--- app/theme/e-apple-store/preview/lib/search.ts
+++ app/theme/e-apple-store/preview/lib/search.ts
@@ -4,7 +4,7 @@ import {products} from './products'
 export default function Search(searchTxt: string, filter: Filter) {
   let searchResults
 
-  if (serachTxt) {
+  if (searchTxt) {
     searchResults = products.filter(product => (product.title.includes(searchTxt) && product.variants.includes(searchTxt)))
     return searchResults
   }
