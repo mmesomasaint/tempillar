1a0e980fb937b4376fd468b950f7b7cb8cd3138b
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-04 23:57:54 +0100

    Fix: Use more appropriate conditionals

--- app/theme/e-apple-store/preview/lib/search.ts
+++ app/theme/e-apple-store/preview/lib/search.ts
@@ -5,7 +5,7 @@ export default function Search(searchTxt: string, filter: Filter) {
   let searchResults
 
   if (searchTxt) {
-    searchResults = products.filter(product => (product.title.includes(searchTxt) && product.variants.includes(searchTxt)))
+    searchResults = products.filter(product => (product.title.includes(searchTxt) || product.variants.includes(searchTxt)))
     return searchResults
   }
 
