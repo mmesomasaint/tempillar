b00e6b6c8179cf896a24ebe7a2329b1f94818767
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-07 08:22:33 +0100

    Create the text variants for the store

---
+++ app/theme/e-fashion-store/page.tsx
@@ -0,0 +1,14 @@
+import { TextBase, TextHeadline, TextIntro, TextMid, TextSmall, TextXSmall } from "./pack/elements/text";
+
+export default function ComponentDisplay() {
+  return (
+    <div>
+      <TextHeadline>Elevate your style with the trends streetwear</TextHeadline>
+      <TextIntro>Our Category</TextIntro>
+      <TextMid>New Couple Clothes Collection</TextMid>
+      <TextBase>Suit Black Panther Cusszz</TextBase>
+      <TextSmall faded>We are a different kind of eccommerce, selling</TextSmall>
+      <TextXSmall>Black Mamba Style Lurr</TextXSmall>
+    </div>
+  )
+}
