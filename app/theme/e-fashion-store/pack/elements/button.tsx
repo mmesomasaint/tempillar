9262ba786692e96c8b4eb6eced21e886dfbfcc9d
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-08 00:44:05 +0100

    Feat: Design the button component for the store

---
+++ app/theme/e-fashion-store/pack/elements/button.tsx
@@ -0,0 +1,7 @@
+export default function Button({children, primary, outlinePri, outlineSec}: {children: string, primary?: boolean, outlinePri?: boolean, outlineSec?: boolean}) {
+  return (
+    <button className={`py-2 px-4 rounded-full shadow-sm border border-fashion-store-sec text-white bg-fashion-store-sec ${primary && 'bg-fashion-store-pri border-fashion-store-pri'} ${outlinePri && 'bg-white text-fashion-store-pri border-fashion-store-pri'} ${outlineSec && 'bg-white text-fashion-store-sec border-fashion-store-sec'}`}>
+      {children}
+    </button>
+  )
+}
