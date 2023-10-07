9570b816f28f88a0243a29a70c7e99c4a53b559c
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-07 08:24:36 +0100

    Feat: Display all the variants for testing

--- app/theme/e-fashion-store/page.tsx
+++ app/theme/e-fashion-store/page.tsx
@@ -4,11 +4,17 @@ export default function ComponentDisplay() {
   return (
     <div>
       <TextHeadline>Elevate your style with the trends streetwear</TextHeadline>
+      <TextHeadline faded>Elevate your style with the trends streetwear</TextHeadline>
       <TextIntro>Our Category</TextIntro>
+      <TextIntro faded>Our Category</TextIntro>
       <TextMid>New Couple Clothes Collection</TextMid>
+      <TextMid faded>New Couple Clothes Collection</TextMid>
       <TextBase>Suit Black Panther Cusszz</TextBase>
+      <TextBase faded>Suit Black Panther Cusszz</TextBase>
+      <TextSmall>We are a different kind of eccommerce, selling</TextSmall>
       <TextSmall faded>We are a different kind of eccommerce, selling</TextSmall>
       <TextXSmall>Black Mamba Style Lurr</TextXSmall>
+      <TextXSmall faded>Black Mamba Style Lurr</TextXSmall>
     </div>
   )
 }
