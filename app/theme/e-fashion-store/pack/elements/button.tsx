5a1236e25562c9f468159e9d44738b29bd5e602f
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-08 00:44:58 +0100

    Fix: Edit to suitable prop type

--- app/theme/e-fashion-store/pack/elements/button.tsx
+++ app/theme/e-fashion-store/pack/elements/button.tsx
@@ -1,4 +1,4 @@
-export default function Button({children, primary, outlinePri, outlineSec}: {children: string, primary?: boolean, outlinePri?: boolean, outlineSec?: boolean}) {
+export default function Button({children, primary, outlinePri, outlineSec}: {children: React.ReactNode, primary?: boolean, outlinePri?: boolean, outlineSec?: boolean}) {
   return (
     <button className={`py-2 px-4 rounded-full shadow-sm border border-fashion-store-sec text-white bg-fashion-store-sec ${primary && 'bg-fashion-store-pri border-fashion-store-pri'} ${outlinePri && 'bg-white text-fashion-store-pri border-fashion-store-pri'} ${outlineSec && 'bg-white text-fashion-store-sec border-fashion-store-sec'}`}>
       {children}
