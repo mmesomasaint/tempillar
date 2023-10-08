ea7bb2c15c5a6ccec83c1e09a8b2aeea97d6c8d4
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-08 22:57:29 +0100

    Fix: Format with prettier

--- app/theme/e-fashion-store/page.tsx
+++ app/theme/e-fashion-store/page.tsx
@@ -1,14 +1,24 @@
-import { BsStar } from "react-icons/bs";
-import Button from "./pack/elements/button";
-import { TextBase, TextHeadline, TextIntro, TextLabel, TextMid, TextSmall, TextXSmall } from "./pack/elements/text";
-import { TbCategoryFilled } from "react-icons/tb";
-import CategoryCard from "./pack/components/card/category";
+import { BsStar } from 'react-icons/bs'
+import Button from './pack/elements/button'
+import {
+  TextBase,
+  TextHeadline,
+  TextIntro,
+  TextLabel,
+  TextMid,
+  TextSmall,
+  TextXSmall,
+} from './pack/elements/text'
+import { TbCategoryFilled } from 'react-icons/tb'
+import CategoryCard from './pack/components/card/category'
 
 export default function ComponentDisplay() {
   return (
     <div>
       <TextHeadline>Elevate your style with the trends streetwear</TextHeadline>
-      <TextHeadline faded>Elevate your style with the trends streetwear</TextHeadline>
+      <TextHeadline faded>
+        Elevate your style with the trends streetwear
+      </TextHeadline>
       <TextIntro>Our Category</TextIntro>
       <TextIntro faded>Our Category</TextIntro>
       <TextMid>New Couple Clothes Collection</TextMid>
@@ -16,37 +26,39 @@ export default function ComponentDisplay() {
       <TextBase>Suit Black Panther Cusszz</TextBase>
       <TextBase faded>Suit Black Panther Cusszz</TextBase>
       <TextSmall>We are a different kind of eccommerce, selling</TextSmall>
-      <TextSmall faded>We are a different kind of eccommerce, selling</TextSmall>
+      <TextSmall faded>
+        We are a different kind of eccommerce, selling
+      </TextSmall>
       <TextXSmall>Black Mamba Style Lurr</TextXSmall>
       <TextXSmall faded>Black Mamba Style Lurr</TextXSmall>
       <Button>
-          <TextLabel>Shop Now</TextLabel>
-          <BsStar className='text-lg' />
+        <TextLabel>Shop Now</TextLabel>
+        <BsStar className='text-lg' />
       </Button>
       <Button primary>
-          <TextLabel>See All</TextLabel>
-          <BsStar className='text-lg' />
+        <TextLabel>See All</TextLabel>
+        <BsStar className='text-lg' />
       </Button>
       <Button outlinePri>
-          <TextLabel>See All</TextLabel>
-          <BsStar className='text-lg' />
+        <TextLabel>See All</TextLabel>
+        <BsStar className='text-lg' />
       </Button>
       <Button outlineSec>
-          <TextLabel>See All</TextLabel>
-          <BsStar className='text-lg' />
+        <TextLabel>See All</TextLabel>
+        <BsStar className='text-lg' />
       </Button>
       <div className='w-1/4 h-60'>
-      <CategoryCard src='/imgs/men-shoes.jpg' title="Shoe" />
+        <CategoryCard src='/imgs/men-shoes.jpg' title='Shoe' />
       </div>
-      <div className="h-[30rem] w-[45rem] grid grid-cols-3 grid-rows-2 gap-3 items-stretch">
-        <div className="row-span-2">
-      <CategoryCard src='/imgs/caucasian-posed-female.jpg' title="Shoe"/>
-      </div>
-      <div className="col-span-2">
-      <CategoryCard src='/imgs/hori-test.jpg' title="decoration"/>
-      </div>
-      <CategoryCard src='/imgs/men-shoes.jpg' title="Shoe" />
-      <CategoryCard src='/imgs/men-shoes.jpg' title="Shoe" />
+      <div className='h-[30rem] w-[45rem] grid grid-cols-3 grid-rows-2 gap-3 items-stretch'>
+        <div className='row-span-2'>
+          <CategoryCard src='/imgs/caucasian-posed-female.jpg' title='Shoe' />
+        </div>
+        <div className='col-span-2'>
+          <CategoryCard src='/imgs/hori-test.jpg' title='decoration' />
+        </div>
+        <CategoryCard src='/imgs/men-shoes.jpg' title='Shoe' />
+        <CategoryCard src='/imgs/men-shoes.jpg' title='Shoe' />
       </div>
     </div>
   )
