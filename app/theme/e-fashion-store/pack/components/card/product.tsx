b236ff727b87e15a2d32b789f6d59d00eb9ba6d8
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-08 22:57:17 +0100

    Fix: Format with prettier

--- app/theme/e-fashion-store/pack/components/card/product.tsx
+++ app/theme/e-fashion-store/pack/components/card/product.tsx
@@ -1,15 +1,23 @@
-import Image from "next/image";
-import { TextXSmall } from "../../elements/text";
-import { BsStar } from "react-icons/bs";
+import Image from 'next/image'
+import { TextXSmall } from '../../elements/text'
+import { BsStar } from 'react-icons/bs'
 
-export default function ProductCard({src, title, price}: {src: string, title: string, price: number}) {
+export default function ProductCard({
+  src,
+  title,
+  price,
+}: {
+  src: string
+  title: string
+  price: number
+}) {
   return (
-    <div className="flex flex-col gap-3 w-full">
-      <div className="relative grow w-full">
-      <Image src={src} fill alt='product image' className="rounded-2xl" />
-      <BsStar className='absolute z-20 text-base text-fashion-store-sec' />
+    <div className='flex flex-col gap-3 w-full'>
+      <div className='relative grow w-full'>
+        <Image src={src} fill alt='product image' className='rounded-2xl' />
+        <BsStar className='absolute z-20 text-base text-fashion-store-sec' />
       </div>
-      <div className="flex justify-between items-center gap-10">
+      <div className='flex justify-between items-center gap-10'>
         <TextXSmall>{title}</TextXSmall>
         <TextXSmall>{`$${price.toString()}`}</TextXSmall>
       </div>
