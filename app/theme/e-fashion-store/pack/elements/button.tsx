62a3920c232823b2600ca35467dc5b84390caf76
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-08 14:42:03 +0100

    Fix: Format with prettier

--- app/theme/e-fashion-store/pack/elements/button.tsx
+++ app/theme/e-fashion-store/pack/elements/button.tsx
@@ -1,16 +1,40 @@
 'use client'
 
-import {ReactNode} from 'react'
+import { ReactNode } from 'react'
 
-export default function Button({children, primary, outlinePri, outlineSec}: {children: ReactNode | ReactNode[], primary?: boolean, outlinePri?: boolean, outlineSec?: boolean}) {
-  const textColor = outlinePri ? 'text-fashion-store-pri' : outlineSec ? 'text-fashion-store-sec' : 'text-white',
-  borderColor = primary || outlinePri ? 'border-fashion-store-pri' : 'border-fashion-store-sec',
-  bgColor = outlinePri || outlineSec ? 'bg-white' : primary ? 'bg-fashion-store-pri' : 'bg-fashion-store-sec'
+export default function Button({
+  children,
+  primary,
+  outlinePri,
+  outlineSec,
+}: {
+  children: ReactNode | ReactNode[]
+  primary?: boolean
+  outlinePri?: boolean
+  outlineSec?: boolean
+}) {
+  const textColor = outlinePri
+      ? 'text-fashion-store-pri'
+      : outlineSec
+      ? 'text-fashion-store-sec'
+      : 'text-white',
+    borderColor =
+      primary || outlinePri
+        ? 'border-fashion-store-pri'
+        : 'border-fashion-store-sec',
+    bgColor =
+      outlinePri || outlineSec
+        ? 'bg-white'
+        : primary
+        ? 'bg-fashion-store-pri'
+        : 'bg-fashion-store-sec'
 
   return (
-    <button className={`py-4 px-7 rounded-full shadow-sm border ${borderColor} ${bgColor}`}>
-    <div className={`flex justify-between items-center gap-4 ${textColor}`}>
-      {children}
+    <button
+      className={`py-4 px-7 rounded-full shadow-sm border ${borderColor} ${bgColor}`}
+    >
+      <div className={`flex justify-between items-center gap-4 ${textColor}`}>
+        {children}
       </div>
     </button>
   )
