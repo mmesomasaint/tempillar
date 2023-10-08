57249af5171ccb0eccb56cf513569b1fddd10485
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-08 22:36:33 +0100

    Feat: Design a collection card component

---
+++ app/theme/e-fashion-store/pack/components/collection-card.tsx
@@ -0,0 +1,12 @@
+import { TextMid, TextXSmall } from "../elements/text";
+
+export default function CollectionCard({bgSrc, title, subTitle}: {bgSrc: string, title: string, subTitle: string}) {
+  return (
+    <div className={`bg-[url('${bgSrc}')] bg-no-repeat bg-contain bg-right bg-white rounded-2xl shadow-sm w-1/2`}>
+      <div className="flex flex-col gap-4">
+      <TextMid faded>{title}</TextMid>
+      <TextXSmall>{subTitle}</TextXSmall>
+      </div>
+    </div>
+  )
+}
