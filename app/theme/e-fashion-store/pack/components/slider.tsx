592c516abc39bfe57cdf3313b0b1ae7d032e38c7
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-09 12:40:11 +0100

    Fix: Format with prettier

--- app/theme/e-fashion-store/pack/components/slider.tsx
+++ app/theme/e-fashion-store/pack/components/slider.tsx
@@ -1,19 +1,26 @@
 'use client'
 
-import { useState, ReactNode } from "react";
-import ProductCard, { ProductCardProps } from "./card/product";
+import { useState, ReactNode } from 'react'
+import ProductCard, { ProductCardProps } from './card/product'
 
-export default function Slider({products}: {products: ProductCardProps[]}) {
+export default function Slider({ products }: { products: ProductCardProps[] }) {
   const [active, setActive] = useState<string>(products[0].title)
 
   return (
-    <div className="relative w-1/2 -right-[25%] p-1">
-      {products.map(product => {
+    <div className='relative w-1/2 -right-[25%] p-1'>
+      {products.map((product) => {
         const isActive = active === product.title
         const activeStyle = 'scale-125'
         const normStyle = 'scale-100'
 
-        return <ProductCard className={`duration-700 ${isActive ? activeStyle : normStyle}`} src={product.src} title={product.title} price={product.price} />
+        return (
+          <ProductCard
+            className={`duration-700 ${isActive ? activeStyle : normStyle}`}
+            src={product.src}
+            title={product.title}
+            price={product.price}
+          />
+        )
       })}
     </div>
   )
