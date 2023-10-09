c8fffdb85557bdd97a4e437861b493a60127c67f
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-09 13:00:38 +0100

    Fix: Format with prettier

--- app/theme/e-fashion-store/pack/components/slider.tsx
+++ app/theme/e-fashion-store/pack/components/slider.tsx
@@ -7,16 +7,17 @@ import { BsStar } from 'react-icons/bs'
 export default function Slider({ products }: { products: ProductCardProps[] }) {
   const [active, setActive] = useState<string>(products[0].title)
   const activeId = useMemo<number>(() => {
-    return (products.findIndex(product => product.title === active))
+    return products.findIndex((product) => product.title === active)
   }, [active])
 
   const getNextActive = (value: string) => {
-    if (activeId !== -1 && activeId < products.length-1 ) products[activeId+1].title
+    if (activeId !== -1 && activeId < products.length - 1)
+      products[activeId + 1].title
     throw new Error('TransitionError: active title not found.')
   }
 
   const getPrevActive = (value: string) => {
-    if (activeId !== -1 && activeId > 1) products[activeId-1].title
+    if (activeId !== -1 && activeId > 1) products[activeId - 1].title
     throw new Error('TransitionError: active title not found.')
   }
 
@@ -29,7 +30,9 @@ export default function Slider({ products }: { products: ProductCardProps[] }) {
 
         return (
           <ProductCard
-            className={`absolute top-0 duration-700 ${isActive ? activeStyle : normStyle}`}
+            className={`absolute top-0 duration-700 ${
+              isActive ? activeStyle : normStyle
+            }`}
             src={product.src}
             title={product.title}
             price={product.price}
@@ -37,10 +40,18 @@ export default function Slider({ products }: { products: ProductCardProps[] }) {
         )
       })}
       <div className='absolute bottom-0 left-[50%] -translate-x-[50%] flex justify-center items-center gap-10'>
-        <button className='w-16 h-16 rounded-full shadow-sm bg-black flex justify-center items-center' onClick={() => setActive(prev => getNextActive(prev))} disabled={activeId === 1}>
+        <button
+          className='w-16 h-16 rounded-full shadow-sm bg-black flex justify-center items-center'
+          onClick={() => setActive((prev) => getNextActive(prev))}
+          disabled={activeId === 1}
+        >
           <BsStar className='text-base text-white' />
         </button>
-        <button className='w-16 h-16 rounded-full shadow-sm bg-black flex justify-center items-center' onClick={() => setActive(prev => getPrevActive(prev))} disabled={activeId === products.length - 1}>
+        <button
+          className='w-16 h-16 rounded-full shadow-sm bg-black flex justify-center items-center'
+          onClick={() => setActive((prev) => getPrevActive(prev))}
+          disabled={activeId === products.length - 1}
+        >
           <BsStar className='text-base text-white' />
         </button>
       </div>
