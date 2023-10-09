91a7e0b99715c0829a7522c7aa9c94f34ce178ec
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-09 12:38:36 +0100

    Feat: Create the product slider component

--- app/theme/e-fashion-store/pack/components/card/product.tsx
+++ app/theme/e-fashion-store/pack/components/card/product.tsx
@@ -2,17 +2,21 @@ import Image from 'next/image'
 import { TextXSmall } from '../../elements/text'
 import { BsStar } from 'react-icons/bs'
 
+export type ProductCardProps = {
+  src: string
+  title: string
+  price: number
+  className?: string
+}
+
 export default function ProductCard({
   src,
   title,
   price,
-}: {
-  src: string
-  title: string
-  price: number
-}) {
+  className
+}: ProductCardProps) {
   return (
-    <div className='flex flex-col gap-3 w-full'>
+    <div className={`flex flex-col gap-3 w-full ${className}`}>
       <div className='relative grow w-full'>
         <Image src={src} fill alt='product image' className='rounded-2xl' />
         <BsStar className='absolute z-20 text-base text-fashion-store-sec' />
