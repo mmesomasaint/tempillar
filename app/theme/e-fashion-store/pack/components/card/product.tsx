88aa2864fbb3e5cc1bce40556a992969307ac4ce
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-10 10:27:55 +0100

    Fix: Adjust the product card

--- app/theme/e-fashion-store/pack/components/card/product.tsx
+++ app/theme/e-fashion-store/pack/components/card/product.tsx
@@ -16,13 +16,13 @@ export default function ProductCard({
   className,
 }: ProductCardProps) {
   return (
-    <div className={`flex flex-col gap-3 w-full ${className}`}>
+    <div className={`flex flex-col gap-3 w-1/6 h-[23rem] ${className}`}>
       <div className='relative grow w-full'>
         <Image src={src} fill alt='product image' className='rounded-2xl' />
-        <BsStar className='absolute z-20 text-base text-fashion-store-sec' />
+        <BsStar className='absolute right-3 top-3 z-20 text-base text-fashion-store-sec' />
       </div>
       <div className='flex justify-between items-center gap-10'>
-        <TextXSmall>{title}</TextXSmall>
+        <TextXSmall copy>{title}</TextXSmall>
         <TextXSmall>{`$${price.toString()}`}</TextXSmall>
       </div>
     </div>
