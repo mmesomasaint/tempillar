8683ebbe6edb3259d4ae5988316ec3ffe7b7962f
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-11 13:07:55 +0100

    Fix: Format with prettier

--- app/theme/e-fashion-store/pack/components/inputbar.tsx
+++ app/theme/e-fashion-store/pack/components/inputbar.tsx
@@ -1,5 +1,22 @@
-export default function InputBar({text, setText, placeholder, full}: {text: string, setText: (text: string) => void, placeholder: string, full?: boolean}) {
+export default function InputBar({
+  text,
+  setText,
+  placeholder,
+  full,
+}: {
+  text: string
+  setText: (text: string) => void
+  placeholder: string
+  full?: boolean
+}) {
   return (
-    <input placeholder={placeholder ?? "Search"} value={text} onChange={e => setText(e.target.value)} className={`border border-fashion-store-outline pl-10 pr-3 py-2 rounded-full bg-white text-base font-medium ${full && 'w-full'}`} />
+    <input
+      placeholder={placeholder ?? 'Search'}
+      value={text}
+      onChange={(e) => setText(e.target.value)}
+      className={`border border-fashion-store-outline pl-10 pr-3 py-2 rounded-full bg-white text-base font-medium ${
+        full && 'w-full'
+      }`}
+    />
   )
 }
