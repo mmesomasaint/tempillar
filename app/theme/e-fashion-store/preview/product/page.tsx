2b7b055deae988dbdb8ea253100c6c2eedcddb7d
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-16 19:35:59 +0100

    Start designing the product page

--- app/theme/e-fashion-store/preview/product/page.tsx
+++ app/theme/e-fashion-store/preview/product/page.tsx
@@ -12,5 +12,13 @@ type ProductProps = {
 }
 
 export default function Product({}: ProductProps) {
-  return <div>Product main page</div>
+  return (
+    <div className='grid grid-cols-12 gap-10'>
+      <div className='col-span-12 md:col-span-6'>
+        <div className='relative w-full h-[30rem]'>
+          
+        </div>
+      </div>
+    </div>
+  )
 }
