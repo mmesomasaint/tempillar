41078e6d3e572295863f704f7c0f7a8a34163161
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-16 20:25:38 +0100

    Fix: Add specific role to page sections

--- app/theme/e-fashion-store/preview/product/page.tsx
+++ app/theme/e-fashion-store/preview/product/page.tsx
@@ -21,8 +21,11 @@ export default function Product({
 }: ProductProps) {
   return (
     <div className='grid grid-cols-12 gap-10'>
-      <div className='col-span-12 md:col-span-6'>
-        <div className='relative w-full h-[30rem]'></div>
+      <div className='col-span-12 md:col-span-3'>
+        <div className='relative w-full h-[30rem]'> Product Image</div>
+      </div>
+      <div className="col-span-12 md:col-span-9">
+        Description and Details
       </div>
     </div>
   )
