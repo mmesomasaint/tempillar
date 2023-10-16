a1cebcf8a3f3ca4197ff8ab0368aed0e81785fb0
Mmesoma Saint <mmesomasaint@gmail.com>
2023-10-16 20:20:50 +0100

    Fix: Format with prettier

--- app/theme/e-fashion-store/preview/product/page.tsx
+++ app/theme/e-fashion-store/preview/product/page.tsx
@@ -11,13 +11,18 @@ type ProductProps = {
   description?: string
 }
 
-export default function Product({id, title, srcSet, price, variants, description}: ProductProps) {
+export default function Product({
+  id,
+  title,
+  srcSet,
+  price,
+  variants,
+  description,
+}: ProductProps) {
   return (
     <div className='grid grid-cols-12 gap-10'>
       <div className='col-span-12 md:col-span-6'>
-        <div className='relative w-full h-[30rem]'>
-          
-        </div>
+        <div className='relative w-full h-[30rem]'></div>
       </div>
     </div>
   )
